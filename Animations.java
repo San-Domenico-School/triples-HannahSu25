1212cbb1be2fa921744a08901fb81941fa01c425
HannahSu25 <xsu25@sandomenico.org>
2025-01-13 11:55:34 +0800

    5.2b

---
+++ Animations.java
@@ -0,0 +1,26 @@
+import greenfoot.*; 
+
+/**
+ * Animation for Triples
+ * 
+ * @Hannah SU  
+ * @Jan 13
+ */
+
+public class Animations  
+{
+    public static void wobble(Card[] cards)
+    {
+        Greenfoot.playSound("wobble.wav");
+    }
+    
+    public static void slideAndTurn(Card[] cards)
+    {
+        Greenfoot.playSound("swoosh.wav");
+        for(int i = 0; i < cards.length; i++)
+        {
+            cards[i].move(-500);
+        }
+    }
+    
+}
---
+++ Scorekeeper.java
@@ -0,0 +1,26 @@
+/**
+ * @Hannah Su
+ * @Jan 13 2025
+ */
+
+public class Scorekeeper  
+{
+    private static int deckSize, score;
+    private static long startTime = System.currentTimeMillis();
+    
+    public static void setDeckSize(int size)
+    {
+        deckSize = size;
+    }
+    
+    public static void updateScore()
+    {   
+        score += Math.abs(27 - (System.currentTimeMillis() - startTime) / 1000);
+        startTime = System.currentTimeMillis();
+    }
+    
+    public static int getScore()
+    {
+        return score;
+    }
+}
